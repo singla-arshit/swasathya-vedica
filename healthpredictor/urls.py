dbdcefd12aaa51cea52fed229dcd7d66526d0a49
अर्शित सिंगला <singlaarshit@gmail.com>
2025-05-27 00:29:45 +0530

    Add files via upload

---
+++ healthpredictor/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for healthpredictor project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    # Admin site
+    path('admin/', admin.site.urls),
+    
+    # Core app URLs
+    path('', include('core.urls')),
+    
+    # Favicon redirect (browsers will look for /favicon.ico)
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
+]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Add debug toolbar if installed
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ImportError:
+        pass
